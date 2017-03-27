ba18eb7b3940ada0e5935b6c92eb9ae33404b0bd
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-03-27 09:51:46 -0600

    almost done, need to make more efficient rendering function and there is one bug I'm not sure how to fix

--- toh_script.rb
+++ toh_script.rb
@@ -11,8 +11,11 @@ class TowerOfHanoi
     @game_board = [[],[],[]] #3 rods
     @rods = [1,2,3]
     @moves = 0
+    @disk_to_move = 0
+    @from = 0
+    @to = 0
+    @error_message = "That is not a valid input, please try again (enter 1, 2, 3 or q to quit)"
   end
-
 
   #game board depends on number of disks specified by user, number of disks is the number of rows
   def game_board
@@ -30,9 +33,80 @@ class TowerOfHanoi
   end
 
 
+  #need method to render the board properly after moves have been made
+  def render
+    rows_to_be_rendered = []
+    (1..@game_board.length).each do |i|
+      row = []
+      @game_board.each do |col|
+        row << (col[-i] || " " * @size_of_disks)
+      end
+      rows_to_be_rendered << row.join(" ") + "\n"
+    end
+    rows_to_be_rendered.reverse.each do |rows_to_be_rendered|
+      print rows_to_be_rendered
+    end
+    print @rods.join("      ") + "\n"
+  end
+
+
+  #get user move from input
+  def get_user_move_from
+    guess_again = true
+    until guess_again == false
+      puts "From which rod would you like to move a disk?"
+      @from = gets.chomp
+      case @from.downcase
+      when "q"
+        puts "Thank you for playing, have a great day!"
+        exit
+      when /[^1-3]/
+        puts @error_message
+        next
+      when /[1-3]/
+        @from = @from.to_i
+        if @game_board[@from - 1][0] == nil
+          puts @error_message
+          next
+        else
+          guess_again = false
+        end
+      end
+    end
+  end
+
+
+  #get user move to input
+  def get_user_move_to
+    guess_again = true
+    until guess_again == false
+      puts "And where would you like to move the disk it to?"
+      @to = gets.chomp
+      case @to.downcase
+      when "q"
+        puts "Thank you for playing, have a great day!"
+        exit
+      when /[^1-3]/
+        puts @error_message
+        next
+      when /[1-3]/
+        @to = @to.to_i
+        if is_move_valid? == true
+          guess_again = false
+          return @to
+        else
+          puts @error_message
+          next
+        end
+      end
+    end
+  end
+
   #check if user input is valid before moving disks
-  def is_move_valid?(from,to, disk_to_move)
-    if (@game_board[from-1] != nil) && ((@game_board[to - 1][0] == nil) || (@game_board[from - 1][0] < @game_board[to - 1][0]))
+  def is_move_valid?
+    if @game_board[@to - 1][0] == nil
+      return true
+    elsif @game_board[@from - 1][0] < @game_board[@to - 1][0]
       return true
     else
       return false
@@ -41,95 +115,50 @@ class TowerOfHanoi
 
 
   #how to move disks based on user input
-  def move_disk(from, to, disk_to_move)
-  #  disk_to_move = @game_board[from - 1][0]
-    @game_board[to - 1].unshift(disk_to_move)
-    @game_board[from - 1].shift
-    print @game_board
-    print @rods.join("    ")
+  def move_disk
+    @disk_to_move = @game_board[@from - 1][0]
+    @game_board[@to - 1].unshift(@disk_to_move)
+    @game_board[@from - 1].shift
   end
 
   #check to see when the game has been won
   def win?
     if (@game_board[1].length == @size_of_disks) || (@game_board[2].length == @size_of_disks)
       puts "You win!!"
-      puts "You won in #{@moves} moves"
+      puts "You won in #{@moves} moves."
       return true
     else
       return false
     end
   end
 
-  #need method to render the board properly after moves have been made
-  def render
-
-
-  end
 
 
   def play
     #instructions for user
     puts "Welcome to the Tower of Hanoi"
     puts "Instructions:"
-    puts "Enter where you would like to move a disk from (either rod number 1, 2 or 3)"
-    puts "Then enter where you would like to move that disk to (either rod number 1, 2, or 3)"
-    puts "You can only move one disc at a time and you cannot place a larger disc on top of a smaller one"
+    print "Enter where you would like to move a disk from (either rod number 1, 2 or 3). "
+    print "Then enter where you would like to move that disk to (either rod number 1, 2, or 3). "
+    print "You can only move one disc at a time and you cannot place a larger disc on top of a smaller one. "
     puts "You have won the game once the entire tower is moved to a different rod or type q to quit"
 
     #display starting game board
     puts game_board
 
-    #get user moves from and to location, move disks
+    #get user moves from and to location, move disks, render, repeat
     until win? == true do
-      puts "From which rod would you like to move a disk from?"
-      from = gets.chomp
-      if from.downcase == "q"
-        break
-      else
-        from = from.to_i
-        if (from > 3) || (from < 1) || (from == nil)
-          puts "That is not a valid move, please re-enter"
-          next
-        end
-      end
-
-      puts "And where would you like to move it to?"
-      to = gets.chomp
-      #logic to exit game if user enters Q or q
-      if to.downcase == "q"
-        break
-      else
-        to = to.to_i
-        if (to > 3) || (to < 1) || (to == nil)
-          puts "That is not a valid move, please re-enter"
-          next
-        end
-      end
-
-      #logic to exit game if user enters Q or q
-      if (from.to_s.downcase == "q") || (to.to_s.downcase == "q")
-        break
-      end
-
-
-      disk_to_move = @game_board[from - 1][0]
-      #as long as user inputs are valid move disks from and to
-      if is_move_valid?(from,to,disk_to_move) == true
-        move_disk(from,to,disk_to_move)
-      else
-        puts "That is not a valid move, please re-enter"
-        next
-      end
+      get_user_move_from
+      get_user_move_to
+      move_disk
+      render
       @moves += 1
     end
 
+  end #end of play
 
 
-  end
+end #end of class
 
-
-
-end
-
-n = TowerOfHanoi.new(4)
+n = TowerOfHanoi.new(3)
 n.play
